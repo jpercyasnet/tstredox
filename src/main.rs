1c75c0cdf91ee06f0fa732cca1950b8d3695c000
jpercyasnet <jpercyasnet@yahoo.com>
2025-07-14 15:21:13 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -36,11 +36,11 @@ fn main() -> iced::Result {
 //     } else {
 //         println!("**ERROR {} get_winsize: {}", errcode, errstring);
      }
-     iced::application(PhotoRot1080::title, PhotoRot1080::update, PhotoRot1080::view)
+     iced::application(PhotoRot1080::update, PhotoRot1080::view)
         .window_size((widthxx, heightxx))
         .theme(PhotoRot1080::theme)
 //        .subscription(ImageList::subscription)
-        .run_with(PhotoRot1080::new)
+        .run()
 
 }
 
